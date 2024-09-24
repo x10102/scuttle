1f806ba3efffc8ea60108e3a2722beb5984f4c0a
x10102 <x10102.ot@gmail.com>
2024-09-24 12:20:03 +0200

    Add meaningful error messages

--- blueprints/autobackup.py
+++ blueprints/autobackup.py
@@ -25,11 +25,12 @@ def run_backup():
     info("Preparing to start backup")
     if not portainer.is_initialized():
         error("Portainer connector is not initialized, can't proceed")
-        return "Internal Error", 500
+        return "Konfigurace Portaineru chybí. Nelze pokračovat.", 500
     try:
         portainer.login()
     except PortainerError as e:
         error(f"Cannot login to portainer ({str(e)})")
+        return "Přihlášení k portaineru selhalo. Nelze pokračovat", 500
     info('Successfully logged in to portainer')
 
     zmq_url = current_app.config.get("WIKICOMMA", None)
@@ -37,7 +38,7 @@ def run_backup():
         zmq_url = zmq_url.get('WCMA_ZMQ_URL', None)
     if not zmq_url:
         error("Wikicomma ZeroMQ URL not set, can't proceed")
-        return "Internal Error", 500
+        return "Chyba: Konfigurace WikiComma chybí.", 500
 
     zmq_thread = Thread(target=zmq_listener_thread, args=(zmq_url,zmq_status,zmq_message_queue), daemon=True)
 
@@ -49,8 +50,8 @@ def run_backup():
         portainer.start_container()
     except PortainerError as e:
         error(f"Couldn't start container, can't proceed ({str(e)})")
-        return "Internal Error", 500
+        return "Chyba: Kontejner nelze spustit", 500
 
     info('Container started')
 
-    return redirect(url_for("AutobackupController.backup_index"))
+    return "Záloha byla spuštěna"
