654c7251df95c7b41498838f912b87c438a2cfd4
x10102 <x10102.ot@gmail.com>
2025-07-03 23:12:33 +0200

    Add result indicator to config ui

--- static/js/backup.js
+++ static/js/backup.js
@@ -38,30 +38,44 @@ async function save_config() {
     await fetch('/backup/config', {
         method: "POST",
         body: JSON.stringify(payload)
+    }).then(async response => {
+        if(response.status == 200) {
+            $("#save-result").removeClass("text-red-600 bg-red-300/5 border-red-300/10")
+            $("#save-result").addClass("text-green-600 bg-green-300/5 border-green-300/10")
+            $("#save-result").text("Konfigurace úspěšně uložena")
+            logMessage("Konfigurace uložena")
+        } else {
+            const reason = await response.text()
+            $("#save-result").removeClassClass("text-green-600 bg-green-300/5 border-green-300/10")
+            $("#save-result").addClass("text-red-600 bg-red-300/5 border-red-300/10")
+            $("#save-result").text(reason)
+            logMessage(`Konfiguraci nelze uložit: ${reason}`)
+        }
+        $("#save-result").fadeIn(500).fadeOut(2000)
     })
 }
 
+// Setup click handlers
 $("#btn-start").on("click", start_backup);
 $("#btn-config").on("click", () => {$('#config-container').fadeToggle()});
 $("#save-config").on("click", save_config)
-last_status_id = -1
-const backupTrigger = document.currentScript.getAttribute('run_url')
 
-function timeStr() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `[${hours}:${minutes}:${seconds}]`;
-}
-
-function logEvent(text) {
-    $('#backup-log').text($('#backup-log').text() + `${timeStr()} ${text}`)
-}
-
-function runBackup() {
-    logEvent("Spouštím zálohu")
-}
+// Load current config into ui
+$(function() {
+    // Hide the save result for now
+    $("#save-result").toggle()
+    // Fetch the config from server and load it into the form fields
+    fetch('/backup/config').then(response => response.json()).then(data => {
+        console.log(data)
+        $("#socks-proxy").val(data.config.socks_proxy)
+        $("#http-proxy").val(data.config.http_proxy)
+        $("#delay").val(data.config.delay)
+        $("#rlimit-size").val(data.config.ratelimit_size)
+        $("#rlimit-refill").val(data.config.ratelimit_refill)
+        $("#blacklist").val(data.config.blacklist)
+        data.wikis.forEach(wiki => $("#wikilist").append(wiki.url+'\n'))
+    })
+})
 
 function setProgressVal(progressbar_id, progress) {
     $(`#${progressbar_id} > div`).css("width", `${progress*100}%`)
