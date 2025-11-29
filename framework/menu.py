ed6242e50d509088e04ea94bdb8428f4a21f5e2a
x10102 <x10102.ot@gmail.com>
2025-11-29 03:08:03 +0100

    Measue sidebar render time

--- framework/menu.py
+++ framework/menu.py
@@ -1,6 +1,7 @@
-from logging import error, critical
+from logging import critical, info
 from .framework import render_template_file
 import yaml
+import time
 import os
 
 # Returns an HTML string containing the app's sidebar menu
@@ -9,6 +10,9 @@ def navigation_menu(authorized: bool = False):
     # Check if we already built the menu
     if hasattr(navigation_menu, '_cache'):
         return navigation_menu._cache[authorized]
+    
+    render_time = time.perf_counter()
+
     try:
         current_dir = os.path.dirname(__file__)
         with open(os.path.join(current_dir, 'config', 'menu.yaml'), 'r', encoding='utf-8') as menufile:
@@ -16,6 +20,7 @@ def navigation_menu(authorized: bool = False):
             menu_config = yaml.safe_load(menufile)
     except Exception as e:
         critical(f"Framework error: failed to render menu ({str(e)})")
+        raise e
 
     # Create the cache attribute on this function
     navigation_menu._cache = ["", ""]
@@ -32,5 +37,7 @@ def navigation_menu(authorized: bool = False):
             navigation_menu._cache[0] += rendered
         if menuitem['auth'] != "unauthorized_only":
             navigation_menu._cache[1] += rendered
+
+    info(f"Pre-rendered navigation menu template in {time.perf_counter()-render_time:.4}s")
 
     return navigation_menu._cache[authorized]
